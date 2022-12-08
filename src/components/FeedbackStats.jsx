fd0d5e3ed96048148cd15346c23436b2964a29d0
amael <amael.cherfaoui@gmail.com>
2022-12-08 22:07:47 +0100

    Manages feedback through context

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({feedback}) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext)
+
   // Calculate ratings avg
   let average = feedback.reduce((acc, cur) => {
     return acc + cur.rating
